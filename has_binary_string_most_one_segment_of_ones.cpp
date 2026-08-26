fbd57bb3ccdc1a5883b82606be19325a12bdfed6
aarshpatel1 <aarsh18patel@gmail.com>
2026-08-26 23:10:15 +0530

    add solution but not test case passing on hackerrank due to hackerrank's test case bug

--- has_binary_string_most_one_segment_of_ones.cpp
+++ has_binary_string_most_one_segment_of_ones.cpp
@@ -6,15 +6,22 @@ int main() {
     std::cout << "enter the binary string: ";
     std::cin >> S;
 
+    // S = "1110000";
+    // S = "0001110000";
+    // S = "00000111";
+    // S = "1001";
+
     int count = 0;
-    for (int i = 0; i < S.length() - 1; i++) {
-        if (S[i] != S[i + 1]) {
-            count++;
-            if (count >= 1) break;
+
+    for (int i = 0; i < S.length(); i++) {
+        if (S[i] == '1') {
+            if (i == 0 || S[i - 1] == '0') {
+                count++;
+            }
         }
     }
 
-    if (count >= 1) std::cout << "false";
+    if (count >= 2) std::cout << "false";
     else std::cout << "true";
 
     return 0;
